82d13d2e36bf0792ede3b2badb4ac351da139360
JKE-code <jayanthkarthik.enaganti@gmail.com>
2025-12-17 23:09:57 +0530

    Delete AIML_Internship directory

--- AIML_Internship/Task_1/palindrome.py
+++
@@ -1,9 +0,0 @@
-# Check palindrome (string or number) - Jayanth
-
-def is_palindrome(value):
-    value = str(value)
-    return value == value[::-1]
-
-value = 121
-print("Is Palindrome:", is_palindrome(value))
-
